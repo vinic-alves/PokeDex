a07bbc3f77b2890f95ce26fc8eef34ad3481559b
vinic-alves <vinicius.alves95@outlook.com>
2023-03-28 20:48:07 -0300

    separando o consumo da API em arquivo diferente

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,19 +29,14 @@ function convertPokemonToLi(pokemon){
 
 const pokemonList = document.getElementById('pokemonList') 
 
-fetch(url)
- .then((response)=> response.json())
- .then((jsonBody)=> jsonBody.results)
- .then((pokemons)=> {
-    
+pokeApi.getPokemons().then((pokemons)=> {
+const listItems = []
     for (let i = 0; i < pokemons.length; i++) {
         const pokemon = pokemons[i];
-        pokemonList.innerHTML += convertPokemonToLi(pokemon)
-
-        
+        listItems.push(convertPokemonToLi(pokemon))
     }
-    
- })
+    console.log(listItems)
+})
 
- .catch((error)=> console.log(error))
+ 
 
