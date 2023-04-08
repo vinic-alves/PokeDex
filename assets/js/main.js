a5d03ed2f40abe33b02d429d63237fb289e76ef0
vinic-alves <vinicius.alves95@outlook.com>
2023-04-08 10:52:31 -0300

    abastração da api para os moldes que necessários da aplicação

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,20 @@
 //requisição da API
 
 
+
 function convertPokemonToLi(pokemon){
 
     return  `
     
-                <li class="pokemon">
-                    <span class="number">#001</span>
-                    <span class="name">${pokemon.name}</span>
+                <li class="pokemon ${pokemon.type}">
+                    <span class="number">#${pokemon.number}</span>
+                    <span class="name"> ${pokemon.name}</span>
                 
                     <div class="detail">
                         <ol class="types">
-                            <li class="type">grass</li>
-                            <li class="type">poison</li>
+                            ${pokemon.types.map((type)=> `<li class="type">${type}</li>`).join('')}
                         </ol>
-                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                        <img src="${pokemon.photo}"
                          alt="${pokemon.name}">
                     </div>
                 </li>
@@ -26,10 +26,8 @@ const pokemonList = document.getElementById('pokemonList')
 
 pokeApi.getPokemons().then((pokemons=[])=> {
 
-
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
-
-    
+    const newHtml =pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml
 
 })
 
---
+++ assets/js/pokemon-model.js
@@ -0,0 +1,9 @@
+
+// nosso modelo para o pokemon, somente o que precisamos para a aplicação.
+class Pokemon{
+    number;
+    name;
+    type;
+    types= [];
+    photo;
+}
